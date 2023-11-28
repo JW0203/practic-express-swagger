2cc71f8f7a34a776fd49941725e7d6305293df6e
JW0203 <widwoo0203@gmail.com>
2023-11-29 01:48:01 +0900

    feat: codes for server

---
+++ app.js
@@ -0,0 +1,14 @@
+onst express = require('express');
+const app = express();
+const port = 3000;
+const userRouter = require('./routes/users')
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swaggerDef');
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use('/users', userRouter);
+
+app.listen(port, () => {
+    console.log(`서버가 실행됩니다. http://localhost:${port}`);
+})
