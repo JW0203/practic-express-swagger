622f5a21e9491a0503022912f1135a6ba00aa533
JW0203 <widwoo0203@gmail.com>
2023-11-29 01:49:32 +0900

    feat: swagger code

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: 유저 정보 조회
+ *     description: 모든 유저 정보를 조회한다.
+ *     responses:
+ *       200:
+ *         description: 조회 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: string
+ *                    description: The user's name
+ */
+
 router.get('/', (req, res) => {
     res.send("유저 정보 조회")
 })
